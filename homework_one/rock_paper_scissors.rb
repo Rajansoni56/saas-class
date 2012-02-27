345213e8631c2f28d0560da739c931907deeea5e
Nicolás Hock Isaza <nhocki@gmail.com>
2012-02-26 21:53:57 -0500

    Commenting

--- homework_one/rock_paper_scissors.rb
+++ homework_one/rock_paper_scissors.rb
@@ -1,19 +1,33 @@
 class WrongNumberOfPlayersError < StandardError ; end
 class NoSuchStrategyError < StandardError ; end
 
+# Verify that the move is P, R or S
 def valid_move?(move)
   %w(p s r).include? move.downcase
 end
 
+# Given two strategies, say if the first player wins
 def wins?(player, opponent)
   player.upcase!
   opponent.upcase!
+
+  # Return true if one of the following:
+  #  * Both players use the same strategy
+  #  * R > S
+  #  * S > P
+  #  * P > R
   (player == opponent) or
   (player == "R" and opponent == "S") or
   (player == "S" and opponent == "P") or
   (player == "P" and opponent == "R")
 end
 
+# Given a match, returns the winner.
+# The match is a 2 position array like this:
+#   [ [name, str], [name, str] ]
+#
+# name: The name of the player
+# str: The strategy used (P, R, S)
 def rps_game_winner(game)
   player, opponent = game.first.last, game.last.last
   raise WrongNumberOfPlayersError unless game.length == 2
@@ -22,6 +36,8 @@ def rps_game_winner(game)
   game.last
 end
 
+# Given a full tournament, returns the winner.
+# The tournament is a binary tree of games.
 def rps_tournament_winner(tournament)
   if tournament.first.first.is_a?(String)
     rps_game_winner(tournament)
