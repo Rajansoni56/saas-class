5f391e2c3de5b15363b8a5a94ef3a236d44c7594
Nicolás Hock Isaza <nhocki@gmail.com>
2012-02-26 22:08:19 -0500

    Commenting

--- homework_one/anagrams.rb
+++ homework_one/anagrams.rb
@@ -1,3 +1,6 @@
+# NOTE: The String#sum method will give you an
+# unique number for the same letters (case sensitive).
+# Use that to get the anagrams
 def combine_anagram(words)
   words.inject({ }) do |hash, word|
     hash[word.downcase.sum] ||= []
