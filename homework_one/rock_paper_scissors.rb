bf8a9b6108de908ee9878740bcf99ee5efe519c7
Nicolás Hock Isaza <nhocki@gmail.com>
2012-02-26 21:49:58 -0500

    Adding the rock-paper-scissors tasks

---
+++ homework_one/rock_paper_scissors.rb
@@ -0,0 +1,46 @@
+class WrongNumberOfPlayersError < StandardError ; end
+class NoSuchStrategyError < StandardError ; end
+
+def valid_move?(move)
+  %w(p s r).include? move.downcase
+end
+
+def wins?(player, opponent)
+  player.upcase!
+  opponent.upcase!
+  (player == opponent) or
+  (player == "R" and opponent == "S") or
+  (player == "S" and opponent == "P") or
+  (player == "P" and opponent == "R")
+end
+
+def rps_game_winner(game)
+  player, opponent = game.first.last, game.last.last
+  raise WrongNumberOfPlayersError unless game.length == 2
+  raise NoSuchStrategyError unless valid_move?(player) and valid_move?(opponent)
+  return game.first if wins?(player, opponent)
+  game.last
+end
+
+def rps_tournament_winner(tournament)
+  if tournament.first.first.is_a?(String)
+    rps_game_winner(tournament)
+  else
+    winner_left  = rps_tournament_winner(tournament.first)
+    winner_right = rps_tournament_winner(tournament.last)
+    rps_tournament_winner([winner_left, winner_right])
+  end
+end
+
+tournament = [
+              [
+               [ ["Armando", "P"], ["Dave", "S"]],
+               [ ["Richard", "R"], ["Michael", "S"]]
+              ],
+              [
+               [ ["Allen", "S"], ["Omer", "P"]],
+               [ ["David E.", "R"], ["Richard X", "P"]]
+              ]
+             ]
+
+p rps_tournament_winner(tournament)
